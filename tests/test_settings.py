b0786ad73303148bd360ccbc8454d74e4a1323b3
Jero Bado <tokidokitalkyou@gmail.com>
2020-05-29 19:44:33 +0800

    Create test_settings.py to test Settings class
    Not yet fully implemented.

---
+++ tests/test_settings.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+import unittest
+from PyQt5.QtCore import Qt
+from PyQt5.QtTest import QTest
+from PyQt5.QtWidgets import QApplication
+from src.core.tenny import Settings
+from src.widget.window import MainWindow
+
+APP = QApplication(sys.argv)
+
+
+class TestSettings(unittest.TestCase):
+
+    def setUp(self):
+
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(levelname)s: %(message)s')
+        self.window = MainWindow()
+
+    def test_saveSettings(self):
+
+        # [] TODO: study how to test settings
+        ...
+
+
+if __name__ == '__main__':
+    unittest.main()
