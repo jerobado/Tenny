d31afe1d3f1504403da3d7f449c0fa0b44c716de
Jero Bado <tokidokitalkyou@gmail.com>
2020-06-05 10:52:06 +0800

    Removed _on_preferencesAction_triggered()
    This menu should only add or remove actions. Let the QAction do the burden of executing a task.

--- src/widget/menu.py
+++ src/widget/menu.py
@@ -11,17 +11,9 @@ class ContextMenu(QMenu):
 
         super().__init__()
         self.preferencesAction = QAction()
-        self.preferencesAction.setText('Preferences')
-        self.preferencesAction.triggered.connect(self._on_preferencesAction_triggered)
+        # [] TODO: create a dedicated Action class to do the lines below
+        # self.preferencesAction = PreferencesAction()
+        # self.quitAction = QuitAction()
 
         self.addAction(self.preferencesAction)
-
-    def _on_preferencesAction_triggered(self):
-
-        from src.dialog.preferences import PreferencesDialog
-
-        dialog = PreferencesDialog()
-        if dialog.exec():
-            logging.debug('settings to be udpated:')
-            logging.debug(f'Start/Stop: {dialog.startstopHotkeyLineEdit.keySequence().toString()}')
-            logging.debug(f'Reset: {dialog.resetHotkeyLineEdit.keySequence().toString()}')
+        # self.addAction(self.quitAction)
